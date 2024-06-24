714f86621d1af27ac035c8a385a1782a00716cc3
Shuhao Li <me@yanksi.li>
2024-06-24 14:03:16 +0200

    new config for half

---
+++ mconfig/dense_half.py
@@ -0,0 +1,11 @@
+# config for training GPT-2 (124M) down to very nice loss of ~2.85 on 1 node of 8X A100 40GB
+# launch as the following (e.g. in a screen session) and wait ~5 days:
+# $ torchrun --standalone --nproc_per_node=8 train.py config/train_gpt2.py
+
+wandb_log = True
+wandb_project = 'GPT2'
+experiment_name='dense'
+
+# Linear layer
+linear_type = "dense"
+n_embd = 540
