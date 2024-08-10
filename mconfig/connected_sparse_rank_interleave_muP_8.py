baa184a5712410be5b94dbc0cdead6c4b4111d10
Shuhao Li <shuhli@student.ethz.ch>
2024-08-10 22:29:27 +0200

    more configs

---
+++ mconfig/connected_sparse_rank_interleave_muP_8.py
@@ -0,0 +1,14 @@
+# config for training GPT-2 (124M) down to very nice loss of ~2.85 on 1 node of 8X A100 40GB
+# launch as the following (e.g. in a screen session) and wait ~5 days:
+# $ torchrun --standalone --nproc_per_node=8 train.py config/train_gpt2.py
+
+wandb_log = True
+# wandb_project = 'GPT2'
+experiment_name='connected_sparse_rank_interleave_muP_8'
+
+# Linear layer
+linear_type = "connected_sparse"
+guarantee_rank = True
+interleave = True
+init_mode = "muP"
+group_size = 8
